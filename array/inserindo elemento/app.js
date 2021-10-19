9d2c961621f2440867b5fda72cacd4de53a1c130
“jaycealbuquerque” <jeycealbuquerquerocha@gmail.com>
2021-10-18 22:05:58 -0300

    // removendo item manualmente

--- array/inserindo elemento/app.js
+++ array/inserindo elemento/app.js
@@ -25,3 +25,22 @@ for (let i = 0; i < numbers.length; i++) {
   numbers[i] = numbers[i + 1]
 }
 console.log(numbers)
+
+// removendo item manualmente
+Array.prototype.reIndex = function (myArray) {
+  const newArray = []
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] !== undefined) {
+      newArray.push(myArray[i])
+    }
+  }
+  return newArray;
+}
+Array.prototype.removeFirstPosition = function () {
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i + 1]
+  }
+  return this.reIndex(this);
+}
+numbers = numbers.removeFirstPosition()
+console.log(numbers)

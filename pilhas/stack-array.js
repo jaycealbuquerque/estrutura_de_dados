3d465196eb0a56bc32c9a391098694f99fbe082c
“jaycealbuquerque” <jeycealbuquerquerocha@gmail.com>
2021-10-25 21:23:24 -0300

    classe stack baseada em objeto

--- pilhas/stack-array.js
+++ pilhas/stack-array.js
@@ -29,3 +29,14 @@ class Stack {
 
 const stack = new Stack()
 console.log(stack.isEmpty())
+
+stack.push(5)
+stack.push(8)
+console.log(stack.peek())
+stack.push(11)
+console.log(stack.size())
+console.log(stack.isEmpty())
+stack.push(15)
+stack.pop()
+stack.pop()
+console.log(stack.size())

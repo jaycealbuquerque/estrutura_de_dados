a6ba94760bad482814220556fa7d45a0af5663a2
“jaycealbuquerque” <jeycealbuquerquerocha@gmail.com>
2021-10-29 00:50:03 -0300

    conversor de base

--- pilhas/stack.js
+++ pilhas/stack.js
@@ -80,3 +80,24 @@ console.log(decimalToBinary(233))
 console.log(decimalToBinary(10))
 console.log(decimalToBinary(1000))
 
+function baseConverter(decNumber, base) {
+  const remStack = new Stack();
+  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let number = decNumber
+  let rem
+  let baseString = ''
+  if (!(base >= 2 && base <= 36)) {
+    return ''
+  }
+  while (number > 0) {
+    rem = Math.floor(number % base)
+    remStack.push(rem)
+    number = Math.floor(number / base)
+  }
+  while (!remStack.isEmpty()) {
+    baseString += digits[remStack.pop()]
+  }
+  return baseString
+}
+
+console.log(baseConverter(115757, 16))
